c1907775da6f526f951e3814839391d7951deaae
tihero <tihero@yandex.ru>
2020-12-13 20:54:34 +0300

    why we block

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import './scss/fonts.scss';
 import './scss/footer.scss';
 import './scss/slider.scss';
 import './scss/services.scss';
+import './scss/why_we.scss';
+
 
 import './js/slider.function';
 import './js/slider_start.js';
